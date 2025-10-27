f92254cc20912bab4842b457f579c124af8bf01f
VitaPan <119960788+VitaPan@users.noreply.github.com>
2025-10-28 06:37:17 +0700

    add structure and header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { MapsHomeWorkFilled } from '@vicons/material'
+</script>
+
+<template>
+<header class="bg-white shadow-sm border-b">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-between items-center py-4">
+        <div class="flex items-center space-x-3">
+          <div class="w-10 h-10 bg-blue-500 rounded-lg flex items-center justify-center">
+            <MapsHomeWorkFilled class="text-white w-6 h-6" />
+          </div>
+          <div class="pl-5">
+            <h1 class="text-xl font-bold text-gray-900">Account Manager</h1>
+            <p class="text-sm text-gray-500">Управление учетными записями</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </header>
+</template>
